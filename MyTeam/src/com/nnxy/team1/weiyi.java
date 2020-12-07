65355adc2db0d0ee358192baabb0e7189b77b4a9
jiangsifang <691887420@qq.com>
2020-12-07 20:38:58 +0800

    第二次修改

--- MyTeam/src/com/nnxy/team1/weiyi.java
+++ MyTeam/src/com/nnxy/team1/weiyi.java
@@ -2,13 +2,27 @@ package com.nnxy.team1;
 
 public class weiyi {
 	private int id;
-	public int getId() {
-		return id;
-		
-	}
+	private String name;
+	private String sex;
 	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
 	public void setId(int id) {
 		this.id = id;
 	}
+	public int getId() {
+		return id;
+	}
+	
 
 }
