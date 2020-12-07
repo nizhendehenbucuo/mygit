bcfdfe3af259f26eb794ad35c3efe8cd43155db8
jiangsifang <691887420@qq.com>
2020-12-07 11:23:33 +0800

    6

--- MyTeam/src/com/nnxy/team6/weishengyu_2.java
+++ MyTeam/src/com/nnxy/team6/weishengyu_2.java
@@ -5,10 +5,10 @@ public class weishengyu_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     
-		System.out.println("hello,xintian");		
-		System.out.println("hello,xintian");
-		System.out.println("hello,xintian");
-		System.out.println("hello,xintian");
+		System.out.println("hello");		
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
 		
 	}
 
