e2ca59e1916f84e89b19a61999e5d01e14eb2c28
jiangsifang <691887420@qq.com>
2020-12-07 12:03:13 +0800

    黄鸿飞 覃由飞_2

--- MyTeam/src/com/nnxy/team5/huanghongfei_2.java
+++ MyTeam/src/com/nnxy/team5/huanghongfei_2.java
@@ -2,4 +2,18 @@ package com.nnxy.team5;
 
 public class huanghongfei_2 {
 
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
