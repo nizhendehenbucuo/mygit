1c0e070930625a7a5b489de9ef3fde42eef3cb2a
jiangsifang <691887420@qq.com>
2020-12-07 11:08:56 +0800

    更新文件

---
+++ MyTeam/src/com/nnxy/team4/pengziyu.java
@@ -0,0 +1,22 @@
+package com.nnxy.team4;
+
+public class pengziyu {
+
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "pengziyu [id=" + id + ", getId()=" + getId() + ", getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + ", toString()=" + super.toString() + "]";
+	}
+	
+	
+}
