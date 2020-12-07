b00bd29aff30844c772ed0a70e5d878d9b4bc16e
jiangsifang <691887420@qq.com>
2020-12-07 11:16:07 +0800

    覃业发

---
+++ MyTeam/src/com/nnxy/team5/git_2.java
@@ -0,0 +1,19 @@
+package com.nnxy.team5;
+
+public class git_2 {
+
+  private int id;
+  
+  
+  public int getId() {
+	  return id;
+	  
+  }
+	
+	public void setId() {
+		this.id = id;
+		
+	}
+	
+	
+}
