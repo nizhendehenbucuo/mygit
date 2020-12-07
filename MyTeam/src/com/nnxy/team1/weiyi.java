411531bc0ad8c7d3b2fa3bab43b94ddcdac74299
jiangsifang <691887420@qq.com>
2020-12-07 20:50:36 +0800

    11111

--- MyTeam/src/com/nnxy/team1/weiyi.java
+++ MyTeam/src/com/nnxy/team1/weiyi.java
@@ -7,19 +7,4 @@ public class weiyi {
 	private String id;
 	private String name;
 	private String sex;
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getSex() {
-		return sex;
-	}
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-	
-
 }
