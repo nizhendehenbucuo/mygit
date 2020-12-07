2da891190eb15db71a397b06e1b1218fe7dce4e8
jiangsifang <691887420@qq.com>
2020-12-07 11:09:26 +0800

    第二次

--- MyTeam/src/com/nnxy/team1/wangxiangyu_2.java
+++ MyTeam/src/com/nnxy/team1/wangxiangyu_2.java
@@ -15,4 +15,10 @@ public String getName() {
 public void setName(String name) {
 	this.name = name;
 }
+@Override
+public String toString() {
+	return "wangxiangyu_2 [id=" + id + ", name=" + name + ", getId()=" + getId() + ", getName()=" + getName()
+			+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+}
+
 }
